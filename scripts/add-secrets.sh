e46c8e2932ae121ccee3ba1677529c3553c1896f
Jim Sheldon <jim.sheldon@gmail.com>
2022-09-02 13:40:39 -0400

    remove desination_image

--- scripts/add-secrets.sh
+++ scripts/add-secrets.sh
@@ -5,11 +5,6 @@ set -euxo pipefail
 cat <<EOF
 ---
 kind: secret
-name: destination_image
-data: $(drone encrypt user-01/dag-setup-verifier ${REGISTRY_NAME}/example/dag-setup-verifier)
-
----
-kind: secret
 name: image_registry_user
 data: $(drone encrypt user-01/dag-setup-verifier ${IMAGE_REGISTRY_USER})
 
